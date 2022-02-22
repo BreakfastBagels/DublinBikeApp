e689bef194e34b3f93ff8574a4a0cff3c8221f34
Jack Mulligan <mulligja1@gmail.com>
2022-02-22 14:21:25 +0000

    Removed API timestamp column from SQL query and associated python code

--- webScraper.py
+++ webScraper.py
@@ -44,15 +44,13 @@ def post_json_to_table(json):
     conn_str = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}"
     engine = create_engine(conn_str, echo=True)
     for row in json:
-        timestamp_updated = datetime.datetime.fromtimestamp((row['last_update']*10**-3))
-        timestamp_updated = f"\'{timestamp_updated}\'"
         timestamp_entered = datetime.datetime.fromtimestamp(time.time())
         timestamp_entered = f"\'{timestamp_entered}\'"
         status = f"\'{row['status']}\'"
-        sql_query = f'''INSERT INTO `dynamic_table` 
-        (Station_Number, Available_Stands, Available_Bikes, Status, Station_Updated, Time_Entered) 
-        VALUES ({row['number']}, {row['available_bike_stands']}, {row['available_bikes']},
-        {status}, {timestamp_updated}, {timestamp_entered});'''
+        sql_query = (
+            f"INSERT INTO `dynamic_table` (Station_Number, Available_Stands, Available_Bikes, Status, Time_Entered) "\
+            f"VALUES ({row['number']}, {row['available_bike_stands']}, {row['available_bikes']},{status}, {timestamp_entered});"\
+        )
         engine.execute(sql_query)
 
 

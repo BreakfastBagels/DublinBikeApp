4b6e8f44780605f917c8eedd77d8dd898f867171
Jack Mulligan <mulligja1@gmail.com>
2022-02-26 18:14:01 +0000

    Set up initial file structure, static files

---
+++ DublinBikeWebApp/web/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from flask import Flask
+# import os
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)

7eed2860916722d612898cdbc0fe2c79de9059c7
Jack Mulligan <mulligja1@gmail.com>
2022-02-27 16:03:38 +0000

    Refactored proof of concept

--- DublinBikeWebApp/web/static/script.js
+++ DublinBikeWebApp/web/static/script.js
@@ -2,8 +2,13 @@ const requestButtons = [...document.getElementsByClassName('request-button')];
 
 requestButtons.forEach((button) => {
     button.addEventListener('click', async () => {
-        const response = await fetch(`/station/${button.id}`);
-        const json = await response.json()
-        console.log(json)
+        const stationJSON = await getJSON(button.id)
+        console.log(stationJSON)
     })
 })
+
+async function getJSON(id) {
+    const response = await fetch(`/station/${id}`);
+    const json = await response.json();
+    return json
+}

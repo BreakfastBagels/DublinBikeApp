c3c86b59a6a9ef28a94728ce8e82327ea8cb6b9b
Ian Foster <fosterian95@gmail.com>
2022-04-01 14:01:50 +0100

    Adding test file for pickle model loading

---
+++ ModellingNotebooks/tryingOutPickleFiles.py
@@ -0,0 +1,11 @@
+import pickle
+from sklearn.preprocessing import PolynomialFeatures
+
+poly = PolynomialFeatures(degree=2)
+
+with open('mean-bikes-pickle3-weekday', 'rb') as file:
+    predictor = pickle.load(file)
+
+for i in range(24):
+    prediction = predictor.predict(poly.fit_transform(([ [i] ])))
+    print(prediction)
